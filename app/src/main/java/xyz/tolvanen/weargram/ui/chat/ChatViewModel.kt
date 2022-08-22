ba49b716d0eea1238cfd1b4b2e57a1a665921490
Arttu Tolvanen <arttu@tolvanen.xyz>
2022-08-22 22:05:03 +0300

    Tweak downloaded photo size

--- app/src/main/java/xyz/tolvanen/weargram/ui/chat/ChatViewModel.kt
+++ app/src/main/java/xyz/tolvanen/weargram/ui/chat/ChatViewModel.kt
@@ -1,11 +1,13 @@
 package xyz.tolvanen.weargram.ui.chat
 
+import android.content.Context
 import android.graphics.BitmapFactory
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.lifecycle.ViewModel
 import androidx.wear.compose.material.ScalingLazyListItemInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -20,9 +22,12 @@ class ChatViewModel @Inject constructor(
     private val client: TelegramClient,
     private val chatProvider: ChatProvider,
     val messageProvider: MessageProvider,
+    @ApplicationContext context: Context
 ) : ViewModel() {
 
     private val TAG = this::class.simpleName
+
+    private val screenWidth = context.resources.displayMetrics.widthPixels
 
     fun initialize(chatId: Long) {
         messageProvider.initialize(chatId)
@@ -56,7 +61,12 @@ class ChatViewModel @Inject constructor(
     }
 
     fun fetchPhoto(photoMessage: TdApi.MessagePhoto): Flow<ImageBitmap?> {
-        return fetchFile(photoMessage.photo.sizes.last().photo)
+        // Take the smallest photo size whose width is larger than screen width,
+        // or the largest available photo size if it doesn't exist
+        val photoSize = photoMessage.photo.sizes.dropWhile { it.width < screenWidth }.firstOrNull()
+            ?: photoMessage.photo.sizes.last()
+
+        return fetchFile(photoSize.photo)
             .map {
                 it?.let {
                     BitmapFactory.decodeFile(it)?.asImageBitmap()
